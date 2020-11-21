81e7283468679e88123146ac186f1e1abab5e232
Jason <stanghelle@gmail.com>
2020-11-21 11:34:24 -0600

    Added final files for submission

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# Chicago-Traffic
+# Chicago Traffic Accident App
+
+##
+This web app created a machine learning model from the City of Chicago's data portal. It estimates the mostly likely type of crash damage and possibility of a hit and run based on the user input route. The model incorporates time of day, day of the week, geolocation and enviornmental factors. User input is run through the model and then creates a accident profile by comparing it to similar accident data.
+
+## Machine Learning Model 
+
+The model relies upon the XGBoost python model. XGBoost stands for extreme gradient boost and employs a gradient boosted tree in its model. The three main predictors for damage and hit and runs were geolocation, lighting conditions and weather conditions. These enviormental factors impacted the model more than day or hour of route. The model had a 51% accuracy for damage type and 73.4% for hit and run profile.
+
+## APP Design
+
+The app was hosted on heroku at https://chicago-accident.herokuapp.com/ using flask. The app incorporated the code from https://github.com/jkgiesler/parse-chicago-neighborhoods/blob/master/gps_to_neighborhood.py to help with locating user routes from user address. Using google maps and the flask-googlemaps module the lat and long of the start and end route were calculated and run through the model. The output code was written directly in the data.html file using flask and jinja.
--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for, redirect
 from flask_googlemaps import GoogleMaps, Map, icons
 from geopy.geocoders import Nominatim
-from config import MAP_API_KEY
-# import requests
+from locate_neighborhood import get_neighborhood
+from joblib import load
+import pandas as pd
+
 app = Flask(__name__)
-GoogleMaps(app, key= MAP_API_KEY
+GoogleMaps(app, key= "AIzaSyDom1FJvz41eWHt9r7e8_tq93_w7Fg8sQA"
 )
 
 @app.route("/")
@@ -23,28 +25,47 @@ def map_created_in_view():
         style="height:400px;width:600px;margin:0;",
     )
 
-    return render_template("index.html", form=form, gmap=gmap)
-
-@app.route('/form')
-def form():
-    return render_template('form.html', form=form)
+    return render_template("index2.html", gmap=gmap)
 
 @app.route("/data", methods=["POST", "GET"])
 def postdata():
-    # Now lat and lon can be accessed as:
-    if request.method == 'GET':
-        return f"The URL /data is accessed directly. Try going to '/form' to submit form"
     if request.method == 'POST':
         form_data = request.form
         geolocator = Nominatim(user_agent="Chicago-Traffic")
-        start_location = geolocator.geocode(form_data["start_address"])
+        start_location = geolocator.geocode(form_data["Start"])
         start_lat = start_location.latitude
         start_long = start_location.longitude
-        end_location = geolocator.geocode(form_data["end_address"])
-        
-        return render_template('data.html',form=form, form_data = form_data, start_location=start_location,
-                               end_location=end_location, start_long=start_long, start_lat=start_lat)
+        end_location = geolocator.geocode(form_data["End"])
+        end_lat = end_location.latitude
+        end_long = end_location.longitude
+        start_hood = get_neighborhood(start_long, start_lat)
+        end_hood = get_neighborhood(end_long, end_lat)
 
+        start_column = f"hood_label_{start_hood}"
+        end_column = f"hood_label_{end_hood}"
+        weather = f"weather_condition_{form_data['weather']}"
+        light_con = f"lighting_condition_{form_data['light_con']}"
+        day = f"crash_day_of_week_{form_data['day_of_week']}"
+        hour = f"crash_hour_{form_data['hour']}"
+
+        input_template = pd.read_csv("input_template.csv")
+        input_template.at[0, start_column] = 1
+        input_template.at[1, end_column] = 1
+        input_template.at[0, weather] = 1
+        input_template.at[1, weather] = 1
+        input_template.at[0, light_con] = 1
+        input_template.at[1, light_con] = 1
+        input_template.at[0, day] = 1
+        input_template.at[1, hour] = 1
+
+        model_damage = load("damage.joblib")
+        damage = model_damage.predict_proba(input_template)
+        model_hit_n_run = load("hit_and_run.joblib")
+        hit_n_run = model_hit_n_run.predict_proba(input_template)
+
+        return render_template("data.html", form_data=form_data, start_location=start_location,
+                               end_location=end_location, start_long=start_long, start_lat=start_lat,
+                               hit_n_run=hit_n_run, damage=damage)
 
 if __name__ == "__main__":
     app.run(debug=True, use_reloader=True)
